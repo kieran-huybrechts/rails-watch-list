fb313dc90619892bf1731ccb87cc5f22f77c1c28
Kieran Huybrechts <kieranhuybrechts@gmail.com>
2023-11-16 14:56:48 +0200

    backend done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,12 +2,33 @@ class BookmarksController < ApplicationController
 
   def new
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
+    # bookmark is linked to list but other than that empty
+    @bookmark = Bookmark.new(list: @list)
     @movies = Movie.all
   end
 
   def create
+    @list = List.find(params['list_id'])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = @list.id
+    @bookmark.movie_id = params['bookmark']['movie_id']
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Movie added to the list.'
+    else
+      @movies = Movie.all
+      render :new
+    end
+  end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,6 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
+
+  resources :bookmarks, only: [:destroy]
 end
